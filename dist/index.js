4f836156ab679f03600c79c37a718b03151ca3fd
Grzegorz Morzycki <gmorzycki@gmail.com>
2018-08-26 15:42:28 +0200

    Transformation to node module

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var dcE = require('./dcE');
+var dgCN = require('./dgCN');
+var dgCNz = require('./dgCNz');
+var dgI = require('./dgI');
+var pARCE = require('./pARCE');
+var pARE = require('./pARE');
+var pCMO = require('./pCMO');
+var pDA = require('./pDA');
+var pGS = require('./pGS');
+var pL = require('./pL');
+var pOTS = require('./pOTS');
+var pSBC = require('./pSBC');
+var pTrim = require('./pTrim');
+
+var exports;
+exports = module.exports;
+
+exports.dcE = dcE;
+exports.dgCN = dgCN;
+exports.dgCNz = dgCNz;
+exports.dgI = dgI;
+exports.pARCE = pARCE;
+exports.pARE = pARE;
+exports.pCMO = pCMO;
+exports.pDA = pDA;
+exports.pGS = pGS;
+exports.pL = pL;
+exports.pOTS = pOTS;
+exports.pSBC = pSBC;
+exports.pTrim = pTrim;
