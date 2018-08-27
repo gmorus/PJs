69e78229cc9e9372996f21532c2329b489c0aeaf
Grzegorz Morzycki <gmorzycki@gmail.com>
2018-08-27 05:54:23 +0200

    External library loading bugfix

--- src/index.js
+++ src/index.js
@@ -13,21 +13,3 @@ export {default as pL} from "./pL";
 export {default as pOTS} from "./pOTS";
 export {default as pSBC} from "./pSBC";
 export {default as pTrim} from "./pTrim";
-
-let PJS = {};
-
-PJS.dcE = dcE;
-PJS.dgCN = dgCN;
-PJS.dgCNz = dgCNz;
-PJS.dgI = dgI;
-PJS.pARCE = pARCE;
-PJS.pARE = pARE;
-PJS.pCMO = pCMO;
-PJS.pDA = pDA;
-PJS.pGS = pGS;
-PJS.pL = pL;
-PJS.pOTS = pOTS;
-PJS.pSBC = pSBC;
-PJS.pTrim = pTrim;
-
-export default PJS;
